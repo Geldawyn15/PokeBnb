9cc5cca14b4c19c2147d9f4c23a085c4f7c7c821
Alexandre Chelednik <cheleds15@gmail.com>
2019-05-21 18:59:48 +0200

    search engine working as intended

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -39,7 +39,24 @@ class PokemonsController < ApplicationController
   end
 
   def search
+    @search = params["search"]
+    query = []
 
+    if @search.present?
+      query << "name= '#{@search['name']}'" if @search["name"] != ''
+
+      query << "poke_type= '#{@search['type'].downcase.capitalize}'" if @search["type"] != ''
+
+      query << "level= '#{@search['level']}'" if @search["level"] != ''
+
+      @pokemons = Pokemon.where(query)
+
+      @pokemons = @pokemons.joins(:transfers).where("transfers.date = ?", @search['date'])if @search["date"] != ''
+
+
+    else
+      @pokemons = Pokemon.all
+    end
   end
 
   private
@@ -53,6 +70,6 @@ class PokemonsController < ApplicationController
   end
 
   def params_pokemon
-    params.require(:pokemon).permit(:name, :poke_type, :anime_url, :image_url, :level)
+    params.require(:pokemon).permit(:name, :poke_type, :anime_url, :image_url, :level, :search)
   end
 end
