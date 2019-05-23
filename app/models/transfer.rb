293ffb23f92e6e119adca75e1a3aabc4b7a17a35
Alexandre Chelednik <cheleds15@gmail.com>
2019-05-23 13:56:15 +0200

    fix heroku

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -2,5 +2,4 @@ class Transfer < ApplicationRecord
   belongs_to :trainer, class_name: "User"
   belongs_to :pokemon
   validates :date, :trainer_id, :pokemon_id, presence: true
-  accepts_nested_attributes_for :reviews
 end
