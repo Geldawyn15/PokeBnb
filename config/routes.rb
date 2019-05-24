325ec7badcf3b0e979351de49859bb992382f52d
JonasChancelier <Jonaschancelier@gmail.com>
2019-05-24 16:38:58 +0200

    Transfer cards good

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   root to: 'pokemons#index', as: 'home'
   resources :users, only: %i[show update] do
     resources :pokemons, only: %i[new create update]
+      resources :transfers, only: %i[new create edit]
   end
 
   resources :pokemons, only: %i[index show edit update destroy]  do
     collection do
       get :search
     end
-    resources :transfers, only: %i[new create edit]
   end
   resources :transfers, only: %i[update destroy]
 end
