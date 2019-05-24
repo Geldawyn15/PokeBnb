3d513fe2dd1ead8f89fc975568b46afac8576dff
JonasChancelier <Jonaschancelier@gmail.com>
2019-05-24 16:56:00 +0200

    correction du trasnfer

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,14 @@ Rails.application.routes.draw do
   root to: 'pokemons#index', as: 'home'
   resources :users, only: %i[show update] do
     resources :pokemons, only: %i[new create update]
-      resources :transfers, only: %i[new create edit]
+      resources :transfers, only: %i[edit]
   end
 
   resources :pokemons, only: %i[index show edit update destroy]  do
     collection do
       get :search
     end
+    resources :transfers, only: %i[new create]
   end
   resources :transfers, only: %i[update destroy]
 end
